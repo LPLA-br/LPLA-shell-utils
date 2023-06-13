8f893865abdc423ddfebfc5bb27ccc8966a36944
Luiz Paulo de Lima Araújo <pluiz0674@gmail.com>
2023-06-13 20:18:03 -0300

    .

--- backup.sh
+++ backup.sh
@@ -7,9 +7,9 @@ EXTRA='/home/luiz/Desktop/bootcamp'
 
 if [[ $(lsblk -o NAME,UUID | grep 'cd474226' | awk '(FS=" "){print $2}') == "cd474226-87e7-4bac-a386-7e07cc59223f" ]]; then
 	if [[ -d $A ]]; then
-		rsync -avh --delete /home/luiz/{bin,Documentos,Imagens,Modelos} $EXTRA /mnt/luiz/
+		rsync -avh --delete --exclude 'node_modules' /home/luiz/{Documentos,Imagens,Modelos,src,Smartphone} $EXTRA /mnt/luiz/
 	else
-		rsync -avh --delete /home/luiz/{bin,Documentos,Imagens,Modelos} /mnt/luiz/
+		rsync -avh --delete --exclude 'node_modules' /home/luiz/{Documentos,Imagens,Modelos,src,Smartphone} /mnt/luiz/
 	fi
 else
 	echo 'dispositivo não é o pendrive de backup';
